1b296dd3ab31dcfa4e114fc0944cc67f471cff25
kenigteh <artemon1002@gmail.com>
2024-01-05 13:52:53 +0200

    ФТ теперь проверяет возможность ввести элемент списка

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,26 +19,35 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Тест: можно начать список и получить его позже"""
-        # Эдит слышала про крутое новое онлайн-приложение со
-        # списком неотложных дел. Она решает оценить его
+        # Эдит слышала про крутое новое онлайн-приложение со списком неотложных дел. Она решает оценить его
         # домашнюю страницу
         self.browser.get('http://localhost:8000')
-        # Она видит, что заголовок и шапка страницы говорят о
-        # списках неотложных дел
+        # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element("tag name", "h1").text
+        self.assertIn('To-Do', header_text)
+
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element("id", "id_new_item")
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби – вязание рыболовных мушек)
-        pass
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница содержит:
         # "1: Купить павлиньи перья" в качестве элемента списка.
-        pass
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element("id", "id_list_table")
+        rows = table.find_elements("tag name", "tr")
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит:
         # "Сделать мушку из павлиньих перьев" (Эдит очень методична)
-        pass
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
         pass
