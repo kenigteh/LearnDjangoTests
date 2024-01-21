52f0ba4e6f19af78cf106df643a17ede71920520
kenigteh <artemon1002@gmail.com>
2024-01-21 19:20:55 +0200

    Добавлен новый путь для создания элементов в списке

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,25 +10,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        """Тест: можно сохранить post-запрос"""
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        """Тест: переадресует после post-запроса"""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/single-list/')
-
-    def test_only_saves_items_when_necessary(self):
-        """Тест: сохраняет элементы, только когда нужно"""
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
     """Тест модели элемента списка"""
@@ -67,3 +48,20 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/single-list/')
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+    '''Тест нового списка'''
+
+    def test_can_save_a_POST_request(self):
+        """Тест: можно сохранить post-запрос"""
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """Тест: переадресует после post-запроса"""
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/single-list/')
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,10 +6,6 @@ from lists.models import Item
 # Создайте здесь представления свои.
 def home_page(request):
     """Домашняя страница"""
-    if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/single-list/')
-
     return render(request, "home.html")
 
 
@@ -17,3 +13,9 @@ def view_list(request):
     """Представление списка"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """Новый список"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/single-list/')
