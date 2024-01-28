696727f125a545a5ce48da8981c2b4933cd30627
kenigteh <artemon1002@gmail.com>
2024-01-28 22:08:59 +0200

    Перемещены ФТ в свои собственные отдельные файлы

---
+++ superlists/functional_tests/base.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common import WebDriverException
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    """Функциональный тест."""
+
+    def setUp(self):
+        """Установка."""
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER', None)
+        if staging_server:
+            self.live_server_url = f'http://{staging_server}'
+
+    def tearDown(self):
+        """Демонтаж."""
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        """Ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element("id", "id_list_table")
+                rows = table.find_elements("tag name", "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
--- superlists/functional_tests/test_simple_list_creation.py
+++ superlists/functional_tests/test_simple_list_creation.py
@@ -1,82 +1,11 @@
-import os
-import time
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.common import WebDriverException
 from selenium.webdriver import Keys
 
-MAX_WAIT = 10
+from functional_tests.base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
     """Тест нового посетителя."""
-
-    def setUp(self):
-        """Установка."""
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER', None)
-        if staging_server:
-            self.live_server_url = f'http://{staging_server}'
-
-    def tearDown(self):
-        """Демонтаж."""
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        """Ожидать строку в таблице списка"""
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element("id", "id_list_table")
-                rows = table.find_elements("tag name", "tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        """Тест: можно начать список и получить его позже"""
-        # Эдит слышала про крутое новое онлайн-приложение со списком неотложных дел. Она решает оценить его
-        # домашнюю страницу
-        self.browser.get(self.live_server_url)
-        # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element("tag name", "h1").text
-        self.assertIn('To-Do', header_text)
-
-        # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element("id", "id_new_item")
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
-        # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби – вязание рыболовных мушек)
-        inputbox.send_keys('Купить павлиньи перья')
-
-        # Когда она нажимает enter, страница обновляется, и теперь страница содержит:
-        # "1: Купить павлиньи перья" в качестве элемента списка.
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-
-        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит:
-        # "Сделать мушку из павлиньих перьев" (Эдит очень методична)
-        inputbox = self.browser.find_element("id", "id_new_item")
-        inputbox.send_keys('Сделать мушку из павлиньих перьев')
-        inputbox.send_keys(Keys.ENTER)
-
-        # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
-        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
-
-        # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что сайт сгенерировал для нее уникальный
-        # URL-адрес – об этом выводится небольшой текст с объяснениями.
-        self.fail('Закончить тест!')
-
-        # Она посещает этот URL-адрес – ее список по-прежнему там.
-        pass
-
-        # Удовлетворенная, она снова ложится спать
 
     def test_can_start_a_list_for_one_user(self):
         """Тест: можно начать список для одного пользователя."""
@@ -153,20 +82,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element("tag name", "body").text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
-
-    def test_layout_and_styling(self):
-        """Тест макета и стилевого оформления"""
-        # Эдит открывает домашнюю страницу
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Она замечает, что поле ввода аккуратно центрировано
-        inputbox = self.browser.find_element("id", "id_new_item")
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
-
-        # Она начинает новый список и видит, что поле ввода там тоже аккуратно центрировано
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element('id', 'id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
