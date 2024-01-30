58054287fe2898b7c0d33c7fb49e24752187d625
kenigteh <artemon1002@gmail.com>
2024-01-30 15:10:04 +0200

    Применена get_absolute_url к модели списка для удаления дублирования url в представлениях (правило DRY)

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 class List(models.Model):
     """Список"""
-    pass
+
+    def get_absolute_url(self):
+        """Получить абсолютный url"""
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
