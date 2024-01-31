fc02267b268a5613a1bcc5d20cee2fd359bdb273
kenigteh <artemon1002@gmail.com>
2024-01-31 18:47:44 +0200

    Добавил новую форму для обработки повторяющихся значений. ExistingListItemForm

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,7 +1,9 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, ExistingListItemForm
 from lists.models import Item, List
 
 
@@ -105,8 +107,21 @@ class ListViewTest(TestCase):
         """Тест отображения формы для элемента"""
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        """Тест: ошибки валидации повторяющегося элемента
+        оканчиваются на странице списков"""
+
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(f'/lists/{list1.id}/', data={'text': 'textey'})
+        expected_error = escape("You've already got this in your list")
+
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
@@ -140,7 +155,7 @@ class NewListTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         """Тест на недопустимый ввод: форма передается в шаблон"""
         response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_invalid_list_items_arent_saved(self):
         """Тест: сохраняются недопустимые элементы списка"""
